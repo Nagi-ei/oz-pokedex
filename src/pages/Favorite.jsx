eaf9014f9c36538703eefadf3fa46b122797e38b
Nagi-ei <nakt4453@gmail.com>
2025-01-15 02:59:33 +0900

    ✨ Implement Favorite component

--- src/pages/Favorite.jsx
+++ src/pages/Favorite.jsx
@@ -4,18 +4,21 @@ import { useSelector } from 'react-redux';
 
 export default function Favorite() {
   const fav = useSelector((state) => state.favorite);
+  const pokedex = useSelector((state) => state.pokedex.data);
+  const favDex = pokedex.filter((poke) => fav.includes(poke.index));
 
   return (
-    <div>
-      {fav.map((i) => (
-        <Card id={i} key={i} />
+    <ul className='flex flex-wrap gap-4'>
+      {favDex.map((poke) => (
+        <Card pokemon={poke} key={poke.index} />
       ))}
-    </div>
+    </ul>
   );
 }
 
 // 굳이 Favorite 페이지가 필요한가
-// 그냥 App.jsx에 showFavorite 상태 하나 줘서 true일때만 필터링하면 안되나
-// 아, 페이지 디테일에 갔다와도 살아있어야함! (?)
+// 그냥 App.jsx에 showFavorite 상태 하나 줘서 true일때만 필터링하면 안되나 (토글)
 
 // 그럼 그냥 처음부터 상태를 배열에 포켓몬 하나당 객체로 {id, 이름, 설명, fav 등등} 으로 만들어서 관리하면 안되나
+
+// 새로고침해도 살아있으려면, favorite을 상태 말고 어딘가 저장해야함.
